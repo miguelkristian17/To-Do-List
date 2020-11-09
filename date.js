8e253c8271810d436aadac2167fdd32a3835adb6
Miguel Martinez <miguelkristian@MacBook-Pro.home>
2020-11-08 18:14:27 -0800

    Version One

---
+++ date.js
@@ -0,0 +1,29 @@
+//jsint esversion:6
+
+exports.getDate = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }; 
+
+    let day = today.toLocaleDateString('en-US', options);
+    return day;
+}
+
+exports.getDay = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+    }; 
+
+    let day = today.toLocaleDateString('en-US', options);
+    return day;
+}
+
+
+
+
